1d2ea5148ea28eee39168d6d30ddd2712ffc3308
Lingat <lingat917@gmail.com>
2026-04-23 22:45:35 +1000

    Added music and videos

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 document.documentElement.setAttribute("data-bs-theme", "dark");
 // A string to store the recent keystrokes
+
 let inputBuffer = "";
 fetch("./assets/tagToMusicMap.json")
   .then((response) => response.json()) // Parse the response as JSON
@@ -7,28 +8,53 @@ fetch("./assets/tagToMusicMap.json")
     let tagKeys = Object.keys(data);
     let inputField = document.querySelector("input");
     const rfidAudio = document.getElementById("rfidAudio");
+    const rfidVideo = document.getElementById("rfidVideo");
+
     const songTitle = document.getElementById("songTitle");
     const songArtist = document.getElementById("songArtist");
     const songImage = document.getElementById("songImage");
+    const playAudio = (src) => {
+      rfidAudio.src = src;
+      rfidAudio.load();
+      rfidAudio.play();
+    };
+
+    const playVideo = (src) => {
+      rfidVideo.src = src;
+      rfidVideo.load();
+      rfidVideo.play();
+    };
+
+    const stopAudio = () => {
+      rfidAudio.src = "";
+      rfidAudio.pause();
+    };
+
+    const stopVideo = () => {
+      rfidVideo.src = "";
+      rfidVideo.pause();
+    };
 
     inputField.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         // 2. Capture the current text
         const currentQuery = inputField.value;
 
-        console.log("Searching for:", currentQuery);
-
         // 3. Perform your logic (e.g., checking tagKeys)
         const upperQuery = currentQuery.toUpperCase();
         if (tagKeys.includes(upperQuery)) {
-          let song = data[upperQuery];
-          songTitle.innerHTML = song.name;
-          songArtist.innerHTML = `By ${song.artist}`;
-          songImage.src = song.imageSrc;
-          rfidAudio.src = song.src;
+          stopVideo();
+          stopAudio();
+          let media = data[upperQuery];
+          songTitle.innerHTML = media.name;
+          songArtist.innerHTML = `By ${media.artist}`;
+          songImage.src = media.imageSrc;
 
-          rfidAudio.load();
-          rfidAudio.play();
+          if (media.src.toLowerCase().endsWith(".mp4")) {
+            playVideo(media.src);
+          } else {
+            playAudio(media.src);
+          }
         } else {
           alert("Tag is not registered");
         }
